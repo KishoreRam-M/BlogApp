e15fbcb841744a0fe88264bde8884ebc34e935f9
KishoreRam-M <mrkisho65@outlook.com>
2025-05-08 18:34:23 +0530

    feat: add security config file still  not  create  jwt and  database

---
+++ src/main/java/com/krm/BlogApplication/Security/SecurityConfig.java
@@ -0,0 +1,61 @@
+package com.krm.BlogApplication.Security;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+@Configuration
+@EnableWebSecurity
+public class  SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .csrf(csrf -> csrf.disable()) // Disable CSRF for APIs
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // Stateless session
+                .authorizeHttpRequests(authority ->
+                        authority
+                                .requestMatchers("/api/**").authenticated() // Secure all /api/** endpoints
+                                .requestMatchers("/auth/signup", "/auth/login").permitAll() // Allow /auth/signup and /auth/login to be accessed without authentication
+                                .anyRequest().permitAll() // Allow other requests without authentication
+                )
+                .addFilterBefore(new JwtTokenValidator(), BasicAuthenticationFilter.class) // Add JWT token validator filter before BasicAuthenticationFilter
+                .cors(cors -> cors.configurationSource(coresConfigurationSource())); // Handle CORS
+
+        return httpSecurity.build();
+    }
+
+
+    private CorsConfigurationSource coresConfigurationSource() {
+        return new CorsConfigurationSource() {
+            @Override
+            public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+                CorsConfiguration configuration = new CorsConfiguration();
+                configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173")); // no trailing slash
+                configuration.setAllowCredentials(true);
+                configuration.setAllowedMethods(Collections.singletonList("*"));
+                configuration.setAllowedHeaders(Collections.singletonList("*"));
+                configuration.setMaxAge(3600L);
+                configuration.setExposedHeaders(Arrays.asList("Authorization"));
+                return configuration;
+            }
+        };
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
